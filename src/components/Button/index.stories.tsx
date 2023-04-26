25faf138dbd1e10e4f637758274391f22b53a0fa
ju12yw <76781368+ju12yw@users.noreply.github.com>
2023-04-27 05:14:24 +0900

    button storybookの修正

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -5,4 +5,34 @@ export default {
   component: Button,
 };
 
-export const HelloButton = () => <Button>Hello World!!!</Button>;
+const Template = (args) => <Button {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Default',
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  children: 'Danger',
+  color: 'danger',
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: 'Primary',
+  color: 'primary',
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  children: 'Small',
+  color: 'primary',
+  size: 'sm',
+};
+
+export const PrimaryLarge = Template.bind({});
+PrimaryLarge.args = {
+  children: 'Large',
+  color: 'primary',
+  size: 'lg',
+};
